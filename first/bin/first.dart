eda1ecf766082b759b4349008f4838de664cc614
yaboytabby <86841460+yaboytabby@users.noreply.github.com>
2024-04-26 06:40:09 -0500

    Mess with async and await.

--- first/bin/first.dart
+++ first/bin/first.dart
@@ -1,32 +1,52 @@
 import 'dart:io';
-
+import 'dart:async';
 class Math {
   num value1 = 0;
   num value2 = 0;
-
+  final fakeLoadingTime = Duration(seconds: 5);
   List<num> get getValues {
-
-    List<num> valList= [value1,value2];
+    List<num> valList = [value1, value2];
     return valList;
   } // Getter and Setter to set some values to hopefully make coding a bit easier.
+
   set setValues(List<num> valList) {
     value1 = valList[0];
     value2 = valList[1];
+    sleep(fakeLoadingTime);
     print("Values set to $valList");
   }
-  num addValues() {
-    return value1 + value2;
+
+  Future<num> addValues() async {
+  // Simulate a potential delay
+  await Future.delayed(Duration(seconds: 1));
+
+  return value1 + value2;
+}
+
+
+  Future<num> subtractValues() async
+  {
+    await Future.delayed((Duration(seconds: 1)));
+
+    return value1 - value2;
+  }
+  Future<num> addThenSubtract() async {
+    num sum = await addValues();
+    await Future.delayed(Duration(seconds: 2));
+    var newDiff = await subtractValues();
+    return newDiff;
+
   }
 }
+
 List<num> buildValueList() {
   print("Choose first value.");
   num v1 = num.parse(stdin.readLineSync()!);
   print("Choose last value.");
   num v2 = num.parse(stdin.readLineSync()!);
-  List<num> builtList = [v1,v2];
+  List<num> builtList = [v1, v2];
   return builtList;
 }
-
 
 void main() {
   var math = Math();
@@ -34,6 +54,10 @@ void main() {
   math.setValues = buildValueList();
 
   print(math.getValues);
-  print(math.addValues());
-
+  print(math.addValues().then((data){ //resolve the future and then print data
+  print(data);
+}));
+  print(math.addThenSubtract().then((data){ //resolve the future and then print data
+  print(data);
+}));
 }
